ef1a3316c7d47a4499dad892a43e728c8ce3d22f
ottovbismarck <fritzj96@gmail.com>
2018-12-09 04:51:22 -0800

    added default navbar

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
