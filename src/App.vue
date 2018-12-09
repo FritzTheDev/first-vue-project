68910fe2362c98cedcd5bdbf637ce51e656fc53a
ottovbismarck <fritzj96@gmail.com>
2018-12-09 05:04:38 -0800

    removed hello-world defaukt component & fixed nav-link size

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <Navbar />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     Navbar
   }
 }
