de156edb7fe4adbdec7ee325fa605efdcc293061
ottovbismarck <fritzj96@gmail.com>
2018-12-09 09:38:43 -0800

    setting up vue-router

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Meta from 'vue-meta'
 import Vuefire from 'vuefire'
 import './firebase'
 
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
 
 import 'bootstrap'
@@ -13,6 +15,7 @@ import './styles/bootstrap-overrides.css'
 Vue.config.productionTip = false
 Vue.use(Meta)
 Vue.use(Vuefire)
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
