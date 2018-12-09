dc6fc5d4ae569fcfc3bb6ea3b4dccc610568541a
ottovbismarck <fritzj96@gmail.com>
2018-12-09 04:21:47 -0800

    My first Vue App. Building it for a client

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
